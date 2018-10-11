16592ca6b19b938b4ef05062c71014ba411e755e
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2018-10-11 14:42:08 +0200

    Add regression test for issue #25
    A regression test is added, confirming the issue that a specific package
    suffers from out of memory errors when being analyzed by Dependency
    Guard.

---
+++ tests/Regression/Issue25/Issue25Test.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright MediaCT. All rights reserved.
+ * https://www.mediact.nl
+ */
+
+namespace Mediact\DependencyGuard\Tests\Regression\Issue25;
+
+use Mediact\DependencyGuard\Tests\Regression\ComposerTestEnvironmentTrait;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+/**
+ * @see https://github.com/mediact/dependency-guard/issues/25
+ */
+class Issue25Test extends TestCase
+{
+    use ComposerTestEnvironmentTrait;
+
+    /**
+     * @return void
+     * @coversNothing
+     */
+    public function testNoOutOfMemoryError(): void
+    {
+        $process = new Process(
+            [
+                PHP_BINARY,
+                '-d',
+                'memory_limit=512M',
+                '../../../bin/dependency-guard'
+            ],
+            __DIR__
+        );
+
+        // The exit code of the process is of no concern to the issue.
+        $process->run();
+
+        $this->assertNotContains(
+            'Allowed memory size',
+            $process->getErrorOutput(),
+            'Dependency guard ran out of memory.'
+        );
+    }
+}
