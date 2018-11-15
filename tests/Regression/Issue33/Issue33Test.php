df7f23a849699efb111a9db558038043d8b89d43
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2018-11-15 09:33:37 +0100

    Add regression test for issue 33
    A regression test is added, replicating the problem with process
    timeouts and/or segmentation faults when the dependency filter is
    applied on circular package links.

---
+++ tests/Regression/Issue33/Issue33Test.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright MediaCT. All rights reserved.
+ * https://www.mediact.nl
+ */
+
+namespace Mediact\DependencyGuard\Tests\Regression\Issue33;
+
+use Mediact\DependencyGuard\Tests\Regression\ComposerTestEnvironmentTrait;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+/**
+ * @see https://github.com/mediact/dependency-guard/issues/33
+ */
+class Issue33Test extends TestCase
+{
+    use ComposerTestEnvironmentTrait;
+
+    /**
+     * @return void
+     * @coversNothing
+     */
+    public function testNoTimeoutOrSegmentationFault(): void
+    {
+        $process = new Process(
+            [
+                PHP_BINARY,
+                '../../../bin/dependency-guard'
+            ],
+            __DIR__
+        );
+
+        $process->setTimeout(5);
+
+        $this->assertEquals(0, $process->run());
+
+        $this->assertNotContains(
+            'Seg',
+            $process->getErrorOutput(),
+            'Dependency guard encountered a segmentation fault.'
+        );
+    }
+}
